83838b58d368712d1a79c8ed23dc430a8f336e3b
silverFox78 <sbarrera@gmail.com>
2017-03-12 17:36:32 -0300

    actualizacion de script

--- js/script.js
+++ js/script.js
@@ -1,43 +1,18 @@
 var Menu = {
     LeerOpciones: function() {
         try {
-            WebService.Consumir(
-                'data/data.json',
-                '{}',
-                function() { Mensajeria.CargardoInicio(); },
-                function(data) {
-                    var obj = data; //$.parseJSON(data.d);
-                    window.console && console.log(obj);
+            $.getJSON('data/data.json', function(data) {
+                var items = [];
+                $.each(data, function(key, val) {
+                    items.push("<li id='" + key + "'>" + val + "</li>");
+                });
 
-                    if (obj.estado) {
-                        $.each(obj.menu,
-                            function(i, item) {
-                                window.console && console.log(item);
-                            }
-                        );
-                    }
+                $("<ul/>", {
+                    "class": "my-new-list",
+                    html: items.join("")
+                }).appendTo("body");
+            });
 
-                    /*if (!obj.estado) {
-                        Error.Alerta('[REP018] Ha ocurrido un error al procesar el reporte, comuniquese con sistemas.', obj.mensaje, true);
-                    } else {
-                        $('#ReporteCabecera').html('');
-
-                        var table = $('#GrillaReporte').DataTable();
-                        table.clear().draw();
-
-                        $.each(obj.objeto.nombrecolumnas,
-                            function (i, item) {
-                                $('#ReporteCabecera').append('<th>' + item + '</th>');
-                            }
-                        );
-                    }*/
-
-                    Mensajeria.CargardoFin();
-                },
-                function(data) {
-                    Error.Alerta("[REP019] No se ha logrado procesar el reporte, comuniquese con sistemas.", data, true);
-                }
-            );
         } catch (e) {
             var mensajeComun = "Error al leer las opciones del menu, comuniquese con el administrador del sitio.";
             var mensajeError = mensajeComun;
